7de2f92569f1926fa716560fd91385091135a6af
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-25 21:10:08 -0500

    adding link to the title

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 
 const AppNavbar = () => {
+  const [showModal, setShowModal] = useState(false);
 
-    const[showModal, setShowModal] = useState(false);
-
-    return (
-        <>
-        <Navbar bg='light' variant='light' expand='lg'>
+  return (
+    <>
+      <Navbar bg="light" variant="light" expand="lg">
         <Container fluid>
-
+          <Navbar.Brand as={Link} to="/">
+            News For You
+          </Navbar.Brand>
         </Container>
-        </Navbar>
-        
-        </>
-    )
+      </Navbar>
+    </>
+  );
 };
-
