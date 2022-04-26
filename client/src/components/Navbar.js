d5ebdd509088e2e4b9a1c405ecb9f88608954323
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-25 20:58:38 -0500

    adding to the navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+
+
+const AppNavbar = () => {
+
+    const[showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+        <Navbar bg='light' variant='light' expand='lg'>
+        <Container fluid>
+
+        </Container>
+        </Navbar>
+        
+        </>
+    )
+};
+
