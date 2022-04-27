37bb508fbb63cdf487f4e55e9fd98e3750f8fd1f
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-27 14:46:15 -0500

    added some styling to the buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Navbar, Container, Nav, Modal, Button, InputGroup, FormControl} from "react-bootstrap";
-import LoginForm from './LoginForm';
+// import LoginForm from './LoginForm';
 
 
 
@@ -25,34 +25,33 @@ const AppNavbar = () => {
       <Nav.Link eventKey={2} >
         Favorites
       </Nav.Link>
-      <Button variant='dark' onClick={handleShow}>Login</Button>
-      {/* <Nav.Link eventKey='login' onClick={() => setShow(true)}>
-        Login
-      </Nav.Link> */}
-      {/* <Nav.Link eventKey='signup' onClick={() => setShow(true)}>
-        Sign Up
-      </Nav.Link> */}
-      <Button variant='secondary' onClick={handleShow}>Signup</Button>
+      <Button variant='dark' onClick={handleShow} size='lg' style={{marginRight: '1rem', padding: '0.7rem 1rem'}}>Login</Button>
+      <Button variant='danger' onClick={handleShow} size="lg" style={{marginRight: '1rem', padding: '0.5rem 1rem'}}>Signup</Button>
     </Nav>
   </Navbar.Collapse>
   </Container>
 </Navbar>
 <Modal size='lg' show={show} onHide={handleClose}>
+    
     <Modal.Header closeButton>
         <Modal.Title>
             Login
         </Modal.Title>
     </Modal.Header>
     <Modal.Body>
-        <InputGroup>
+        <InputGroup className='mb-3'>
             <InputGroup.Text>Email</InputGroup.Text>
             <FormControl type='email' />
         </InputGroup>
-        <InputGroup>
+        <InputGroup className='mb-3'>
             <InputGroup.Text>Password</InputGroup.Text>
             <FormControl type='password' />
         </InputGroup>
     </Modal.Body>
+    <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>Close</Button>
+        <Button variant='primary'>Login</Button>
+    </Modal.Footer>
 </Modal>
 
 
