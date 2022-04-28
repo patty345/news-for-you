74827d488c22be6dc96448dc1a1563e7fe9e7533
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-27 19:01:27 -0500

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,102 @@
 import React, { useState } from "react";
-import { Navbar, Container, Nav, Modal, Button, InputGroup, FormControl} from "react-bootstrap";
-// import LoginForm from './LoginForm';
-
-
-
+import {
+  Navbar,
+  Container,
+  Nav,
+  Modal,
+  Button,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import LoginForm from "./LoginForm";
+// import SignUpForm from "./SignupForm";
 
 const AppNavbar = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-  <Container>
-  <Navbar.Brand href="/">News For You</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-    </Nav>
-    <Nav>
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link eventKey={2} >
-        Favorites
-      </Nav.Link>
-      <Button variant='dark' onClick={handleShow} size='lg' style={{marginRight: '1rem', padding: '0.7rem 1rem'}}>Login</Button>
-      <Button variant='danger' onClick={handleShow} size="lg" style={{marginRight: '1rem', padding: '0.5rem 1rem'}}>Signup</Button>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-<Modal size='lg' show={show} onHide={handleClose}>
-    
+        <Container>
+          <Navbar.Brand href="/">News For You</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link eventKey={2}>Favorites</Nav.Link>
+              <Button
+                variant="dark"
+                onClick={handleShow}
+                size="lg"
+                style={{ marginRight: "1rem", padding: "0.7rem 1rem" }} 
+              >
+                Login
+              </Button>
+              <Button
+                variant="danger"
+                onClick={handleShow}
+                size="lg"
+                style={{ marginRight: "1rem", padding: "0.5rem 1rem" }} 
+              >
+                Signup
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+          <Modal size="lg" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Login</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Email</InputGroup.Text>
+                <FormControl type="email" />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Password</InputGroup.Text>
+                <FormControl type="password" />
+              </InputGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary">Login</Button>
+            </Modal.Footer>
+          </Modal>
+          <Modal size='lg' show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+        <Modal.Title>
+            Signup
+        </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+    <InputGroup className='mb-3'>
+            <InputGroup.Text>Username</InputGroup.Text>
+            <FormControl type='username' />
+        </InputGroup>
+        <InputGroup className='mb-3'>
+            <InputGroup.Text>Email</InputGroup.Text>
+            <FormControl type='email' />
+        </InputGroup>
+        <InputGroup className='mb-3'>
+            <InputGroup.Text>Password</InputGroup.Text>
+            <FormControl type='password' />
+        </InputGroup>
+    </Modal.Body>
+    <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>Close</Button>
+        <Button variant='primary'>Submit</Button>
+    </Modal.Footer>
+</Modal>
+        </Container>
+      </Navbar>
+      {/* <Modal size='lg' show={show} onHide={handleClose}>
     <Modal.Header closeButton>
         <Modal.Title>
             Login
@@ -52,9 +116,32 @@ const AppNavbar = () => {
         <Button variant='secondary' onClick={handleClose}>Close</Button>
         <Button variant='primary'>Login</Button>
     </Modal.Footer>
-</Modal>
-
-
+</Modal> */}
+      {/* <Modal size='lg' show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+        <Modal.Title>
+            Signup
+        </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+    <InputGroup className='mb-3'>
+            <InputGroup.Text>Username</InputGroup.Text>
+            <FormControl type='username' />
+        </InputGroup>
+        <InputGroup className='mb-3'>
+            <InputGroup.Text>Email</InputGroup.Text>
+            <FormControl type='email' />
+        </InputGroup>
+        <InputGroup className='mb-3'>
+            <InputGroup.Text>Password</InputGroup.Text>
+            <FormControl type='password' />
+        </InputGroup>
+    </Modal.Body>
+    <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>Close</Button>
+        <Button variant='primary'>Submit</Button>
+    </Modal.Footer>
+</Modal> */}
     </>
   );
 };
