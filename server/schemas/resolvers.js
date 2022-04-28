d68c5c66c48924abb1b988adfe35e24d9ae11ccc
Zane Scheel <zane.scheel@gmail.com>
2022-04-27 19:55:38 -0500

    created starter mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,30 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { update } = require('../models/User');
 
 const resolvers = {
     Query: {
         users: async () => {
             return User.find()
                 .select('-__v -password')
-                .populate('favoriteArticles');
         },
         user: async (parent, { username }) => {
             return User.findOne({ username })
                 .select('-__v -password')
-                .populate('favoriteArticles');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            return { user }
+        },
+        addArticle: async (parent, { userId, articleBody }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: userId },
+                { $push: { favoriteArticles: { articleBody }}},
+                { new: true }
+            );
+            return updatedUser;
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,11 +5,30 @@ const typeDefs = gql`
         _id: ID
         username: String
         email: String
-    },
+        password: String
+        favoriteArticles: [Article]
+    }
+
+    type Article {
+        _id: ID
+        publisher: String
+        title:  String
+        description: String
+        content: String
+        url: String
+        urlToImage: String
+        publishedAt: String
+    }
+
     type Query {
         users: [User]
         user(username: String!): User
-      }
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): User
+        addArticle(userId: ID!, articleBody: String): User
+    }
 `
 
 module.exports = typeDefs;
