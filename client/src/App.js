df9c7b14fe6040c13a4b4a9e336c748b7fa61046
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-27 20:33:52 -0500

    added the newscontent

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
 import Navbar from './components/Navbar';
+import NewsContent from './components/Newscontent';
 import SignupForm from './components/SignupForm';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <SignupForm></SignupForm>
       <LoginForm></LoginForm>
+      <NewsContent></NewsContent>
     </div>
     </>
     
---
+++ client/src/components/Newscontent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Row, Card, Col } from 'react-bootstrap';
+
+
+const NewsContent = () => {
+
+
+    return(
+        <Row xs={1} md={2} className="g-4">
+  {Array.from({ length: 4 }).map((_, idx) => (
+    <Col style={{ padding: '5rem'}}>
+      <Card>
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  ))}
+</Row>
+    )
+}
+
+export default NewsContent;
