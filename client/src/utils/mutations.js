d4e2ec70165ed5a3bc86f02e914ed58099220aa4
Zane Scheel <zane.scheel@gmail.com>
2022-04-29 12:59:37 -0500

    finished mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client'
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token 
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_THOUGH = gql `
+mutation addArticle($username: String!, $publisher: String!, $title: String!, $description: String!, $content: String!, $url: String!, $urlToImage: String!, $publishedAt: String!) {
+    addArticle(username: $username, publisher: $publisher, title: $title, description: $description, content: $content, url: $url, urlToImage: $urlToImage, publishedAt: $publishedAt) {
+        favoriteArticle {
+            _id: ID
+            publisher: String
+            title:  String
+            description: String
+            content: String
+            url: String
+            urlToImage: String
+            publishedAt: String
+        }
+    }
+}`
