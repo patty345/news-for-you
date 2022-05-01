14d4834b4491097c6566da447200407462e60521
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-30 20:24:22 -0500

    added the .env file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const searchArticles = (query, page) => {
-    return fetch(`https://newsapi.org/v2/everything?q=${query}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=API_KEY`);
-}
+  return fetch(
+    `https://newsapi.org/v2/everything?q=${query}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=API_KEY`
+  );
+};
