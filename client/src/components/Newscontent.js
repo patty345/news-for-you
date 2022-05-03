0f2ebd9fd8a75a3292e9fc96264da44f3d1c0fa6
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-05-02 20:49:29 -0500

    api test

--- client/src/components/Newscontent.js
+++ client/src/components/Newscontent.js
@@ -1,38 +1,51 @@
-import React from "react";
-// import newsAPI from '../utils/API';
+import React, { useState } from "react";
 import { Row, Card, Col, Button } from "react-bootstrap";
-import { searchArticles } from "../utils/API";
+import { searchArticles }from "../utils/API";
 
-const NewsContent = async () => {
-  const response = await searchArticles;
 
-  if (!response.ok) {
-    throw new Error("something went wrong");
-  }
 
-  const { items } = await response.json();
+const NewsContent = () => {
 
-  const newsData = items.map((news) => ({
-    newsId: news._id,
-    publisher: news.publisher,
-    title: news.title,
-    description: news.description,
-    content: news.content,
-    urlToImage: news.urlToImage,
-  }));
+    const [articles, setArticles] = useState([])
 
+    
+  
+    const handleArticles = async () => {
+
+        const response = await searchArticles(1);
+    
+      if (!response.ok) {
+        throw new Error("something went wrong");
+      }
+    
+      const { items } = await response.json();
+
+
+    
+      const newsData = items.map((news) => ({
+        newsId: news._id,
+        publisher: news.publisher,
+        title: news.title,
+        description: news.description,
+        content: news.content,
+        urlToImage: news.urlToImage,
+      }));
+
+      setArticles(newsData)
+    }
+    handleArticles()
   return (
     <Row xs={1} md={2} className="g-4">
-      {newsData.map((news, idx) => (
+      {articles.map((news, idx) => (
         <Col style={{ padding: "5rem" }}>
           <Card>
             <Card.Img
               variant="top"
-              src="https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+              src={news.urlToImage}
             />
             <Card.Body>
               <Card.Title>{news.title}</Card.Title>
-              <Card.Text>{news.text}</Card.Text>
+              <Card.Text>{news.description}</Card.Text>
               <Button variant="info">Save to Favorites</Button>
             </Card.Body>
           </Card>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
-export const searchArticles = (query, page) => {
+export const searchArticles = (page) => {
   return fetch(
-    `https://newsapi.org/v2/everything?q=${query}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
+    `https://newsapi.org/v2/everything?pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
   );
 };
