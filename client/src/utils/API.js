3e1846c3d1fad8e4c74f43b0d15b51cf0c81971b
Zane Scheel <zane.scheel@gmail.com>
2022-05-02 19:24:00 -0500

    added auth to the frontend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 export const searchArticles = (query, page) => {
   return fetch(
-    `https://newsapi.org/v2/everything?q=${query}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${proccess.env.REACT_APP_API_KEY}`
+    `https://newsapi.org/v2/everything?q=${query}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
   );
 };
