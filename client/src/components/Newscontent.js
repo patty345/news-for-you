e620e4b51683b879c126d92ddc71a58c6e08e28b
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-05-02 19:46:08 -0500

    api error

--- client/src/components/Newscontent.js
+++ client/src/components/Newscontent.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React from "react";
 // import newsAPI from '../utils/API';
-import { Row, Card, Col, Button } from 'react-bootstrap';
+import { Row, Card, Col, Button } from "react-bootstrap";
+import { searchArticles } from "../utils/API";
 
+const NewsContent = async () => {
+  const response = await searchArticles;
 
-const NewsContent = () => {
+  if (!response.ok) {
+    throw new Error("something went wrong");
+  }
 
-    const newsData = [
-        {"img": "", title: "title01", text:"text1"},
-        {"img": "", title: "title02", text:"text2"},
-        {"img": "", title: "title03", text:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."}
-    ]
+  const { items } = await response.json();
 
-    return(
-        <Row xs={1} md={2} className="g-4">
-  {newsData.map((news, idx) => (
-    <Col style={{ padding: '5rem'}}>
-      <Card>
-        <Card.Img variant="top" src="https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
-        <Card.Body>
-          <Card.Title>{news.title}</Card.Title>
-          <Card.Text>
-            {news.text}
-          </Card.Text>
-          <Button variant='info'>Save to Favorites</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  ))}
-</Row>
-    )
-}
+  const newsData = items.map((news) => ({
+    newsId: news._id,
+    publisher: news.publisher,
+    title: news.title,
+    description: news.description,
+    content: news.content,
+    urlToImage: news.urlToImage,
+  }));
+
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {newsData.map((news, idx) => (
+        <Col style={{ padding: "5rem" }}>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            />
+            <Card.Body>
+              <Card.Title>{news.title}</Card.Title>
+              <Card.Text>{news.text}</Card.Text>
+              <Button variant="info">Save to Favorites</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default NewsContent;
