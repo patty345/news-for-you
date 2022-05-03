2581c11d6eb1e5d5204ab38ebd6b9243556a1970
Zane Scheel <zane.scheel@gmail.com>
2022-05-02 20:19:54 -0500

    fixed error

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const searchArticles = (query, page) => {
   return fetch(
     `https://newsapi.org/v2/everything?q=${query}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
